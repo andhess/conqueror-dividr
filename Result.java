32f57f1312e662fb9d2fa263ab0da1c3384b7f54
andhess <andrewclaytonhess@gmail.com>
2012-09-18 12:48:54 -0500

    added basic lab, now time to get to work

---
+++ Result.java
@@ -0,0 +1,15 @@
+package lab1new;
+
+public class Result {
+    
+    XYPoint p1;
+    XYPoint p2;
+    double dist;
+    
+    public Result(XYPoint ip1, XYPoint ip2, double idist)
+    {
+	p1 = ip1;
+	p2 = ip2;
+	dist = idist;
+    }
+}
