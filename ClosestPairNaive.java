4a9b66c4f7232ef70cae7eb390a8f07885aa75d5
andhess <andrewclaytonhess@gmail.com>
2012-09-24 18:47:40 -0500

    finished naive set

--- ClosestPairNaive.java
+++ ClosestPairNaive.java
@@ -16,12 +16,28 @@ public class ClosestPairNaive {
     //  - points sorted in nondecreasing order by Y coordinate
     //
     
-    public static void findClosestPair(XYPoint points[])
-    {
+    public static void findClosestPair( XYPoint points[] ){
 	int nPoints = points.length;
+	double minDist = INF;
+	XYPoint point1;
+	XYPoint point2;
 	
-	//
-	// Your code goes here!
-	//
+	for( int i = 0; i <= nPoints - 1; i++ ){
+		for( int j = 1; j <= nPoints; j++ ){
+			double dist = points[i].dist( points[j] );
+			if( minDist > dist ){
+				minDist = dist;
+				if( points[i].x < points[j].x ){
+					point1 = points[i];
+					point2 = points[j];
+				}
+				else{
+					point1 = points[j];
+					point2 = points[i];
+				}
+			}
+		}
+	}
+	System.out.println( point1 + ", " + point2 + " are the closest pair at a distance of:  " + minDist );
     }
 }
